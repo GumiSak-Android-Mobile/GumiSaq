21a273a3d0b967e1ad7e3ac756b7196592d49cf5
muhammaddzaki123 <muhammaddzaki933@gmail.com>
2025-06-02 17:21:45 +0800

    push sebagian dulu belom lengkap tunggu ovi

---
+++ constants/images.ts
@@ -0,0 +1,12 @@
+import baju from "@/assets/images/baju.png";
+import logoawal from "@/assets/images/logoawal.png";
+import whiteGradient from "@/assets/images/white-gradient.png";
+import cardgradient from "@/assets/images/card-gradient.png";
+
+
+export default {
+  whiteGradient,
+  logoawal,
+  baju,
+  cardgradient,
+};
