eeb20d45b8b6de94393073fc6b58ad53ce6e5188
Abidi944 <apriadiabdi4@gmail.com>
2025-06-02 22:17:47 +0800

    com

---
+++ constants/data.ts
@@ -0,0 +1,160 @@
+import images from "./images";
+export const cards = [
+  {
+    title: "Baju ovi",
+    location: "Lombok",
+    price: "Rp.1000.000",
+    rating: 4.8,
+    category: "baju",
+    image: images.baju,
+  },
+  {
+    title: "Baju Abdi",
+    location: "Location 2",
+    price: "Rp.1000.000",
+    rating: 3,
+    category: "baju",
+    image: images.baju,
+  },
+  {
+    title: "Baju ario",
+    location: "lotim",
+    price: "Rp.1000.000",
+    rating: 2,
+    category: "baju",
+    image: images.baju,
+  },
+  {
+    title: "Card 4",
+    location: "narmada",
+    price: "Rp.1000.000",
+    rating: 5,
+    category: "baju",
+    image: images.baju,
+  },
+];
+
+export const featuredCards = [
+  {
+    title: "Featured 1",
+    location: "Location 1",
+    price: "$100",
+    rating: 4.8,
+    image: images.baju,
+    category: "obat anak",
+  },
+  {
+    title: "Featured 2",
+    location: "Location 2",
+    price: "$200",
+    rating: 3,
+    image: images.baju,
+    category: "obat",
+  },
+];
+
+export const categories = [
+  { title: "All", category: "All" },
+  { title: "Propolist", category: "obat" },
+  { title: "skin care", category: "skin care" },
+  { title: "facial wash", category: "sabun" },
+  { title: "propolist anak", category: "obat anak" },
+  { title: "Others", category: "Others" },
+];
+
+// export const settings = [
+//   {
+//     title: "My Bookings",
+//     icon: icons.calendar,
+//   },
+//   {
+//     title: "Payments",
+//     icon: icons.wallet,
+//   },
+//   {
+//     title: "Profile",
+//     icon: icons.person,
+//   },
+//   {
+//     title: "Notifications",
+//     icon: icons.bell,
+//   },
+//   {
+//     title: "Security",
+//     icon: icons.shield,
+//   },
+//   {
+//     title: "Language",
+//     icon: icons.language,
+//   },
+//   {
+//     title: "Help Center",
+//     icon: icons.info,
+//   },
+//   {
+//     title: "Invite Friends",
+//     icon: icons.people,
+//   },
+// ];
+
+// export const facilities = [
+//   {
+//     title: "Laundry",
+//     icon: icons.laundry,
+//   },
+//   {
+//     title: "Car Parking",
+//     icon: icons.carPark,
+//   },
+//   {
+//     title: "Sports Center",
+//     icon: icons.run,
+//   },
+//   {
+//     title: "Cutlery",
+//     icon: icons.cutlery,
+//   },
+//   {
+//     title: "Gym",
+//     icon: icons.dumbell,
+//   },
+//   {
+//     title: "Swimming pool",
+//     icon: icons.swim,
+//   },
+//   {
+//     title: "Wifi",
+//     icon: icons.wifi,
+//   },
+//   {
+//     title: "Pet Center",
+//     icon: icons.dog,
+//   },
+// ];
+
+// export const gallery = [
+//   {
+//     id: 1,
+//     image: images.newYork,
+//   },
+//   {
+//     id: 2,
+//     image: images.japan,
+//   },
+//   {
+//     id: 3,
+//     image: images.newYork,
+//   },
+//   {
+//     id: 4,
+//     image: images.japan,
+//   },
+//   {
+//     id: 5,
+//     image: images.newYork,
+//   },
+//   {
+//     id: 6,
+//     image: images.japan,
+//   },
+// ];
---
+++ constants/images.ts
@@ -0,0 +1,12 @@
+import baju from "@/assets/images/baju.png";
+import cardgradient from "@/assets/images/card-gradient.png";
+import logoawal from "@/assets/images/logoawal.png";
+import whiteGradient from "@/assets/images/white-gradient.png";
+
+
+export default {
+  whiteGradient,
+  logoawal,
+  baju,
+  cardgradient,
+};
